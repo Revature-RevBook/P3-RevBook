32b905e320ea3db66bf305386f69fd5c89b3a99a
HaruyaMaeda <hmaeda022@gmail.com>
2022-06-07 15:14:05 -0500

    Added basic crud functionality of post

--- src/main/java/org/revature/revbook/data/PostRepository.java
+++ src/main/java/org/revature/revbook/data/PostRepository.java
@@ -1,4 +1,9 @@
 package org.revature.revbook.data;
 
-public interface PostRepository {
+import org.revature.revbook.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
