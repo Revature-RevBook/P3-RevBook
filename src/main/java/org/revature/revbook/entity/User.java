c7551ebf3cb3289030afba9996f8c91d1b4a13e3
Priyanka Vemuri <vemuripriyanka0317@gmail.com>
2022-06-09 14:52:01 -0500

    login back end

--- src/main/java/org/revature/revbook/entity/User.java
+++ src/main/java/org/revature/revbook/entity/User.java
@@ -1,14 +1,74 @@
 package org.revature.revbook.entity;
 
-import javax.persistence.Table;
 
-// Lombok annotations
-// annotate here...
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@ToString
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     // Data members for the User object:
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    @Column(unique = true)
+    private String username;
+    @Column(unique = true)
+    private String eMail;
+    private String password;
 
-    // Constructor without ID:
+    //Constructor without ID:
+    public User(String username, String eMail, String password  ) {
+        this.username = username;
+        this.eMail = eMail;
+        this.password = password;
 
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    // IsAccountNonExpired method
+    // This method will return if the user account is expired:
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    // IsAccountNonLocked method
+    // This method will return if the user account is locked:
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    // IsCredentialsNonExpired method
+    // This method will return if the user account's credentials are expired:
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // IsEnabled method
+    // This method will return if the user account is enabled or not:
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
