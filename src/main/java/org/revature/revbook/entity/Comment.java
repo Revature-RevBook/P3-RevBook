656d5dc11875ce312f1153534cdfd01048a55b7c
drtimoth <david825@revature.net>
2022-06-09 11:33:53 -0700

    Basic post/get/delte
    No sub comments

--- src/main/java/org/revature/revbook/entity/Comment.java
+++ src/main/java/org/revature/revbook/entity/Comment.java
@@ -1,5 +1,31 @@
 package org.revature.revbook.entity;
 
-public class Comment {
+import javax.persistence.Entity;
 
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String commentTxt;
+    @Column(name = "creation_date")
+    @CreationTimestamp
+    private Timestamp currentDate;
 }
--- src/main/java/org/revature/revbook/service/CommentService.java
+++ src/main/java/org/revature/revbook/service/CommentService.java
@@ -1,4 +1,32 @@
 package org.revature.revbook.service;
 
+import java.util.List;
+
+import org.revature.revbook.data.CommentRepository;
+import org.revature.revbook.entity.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class CommentService {
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    public Comment postComment(Comment comment){
+        commentRepository.save(comment);
+        return comment;
+    }
+
+    public List<Comment> get_all_comments() {
+        return commentRepository.findAll();
+    }
+    public Comment get_by_id(long id) {
+        return commentRepository.findById(id).get();
+    }
+
+    public void delete_comment(Long id) {
+        commentRepository.deleteById(id);
+    }
 }
