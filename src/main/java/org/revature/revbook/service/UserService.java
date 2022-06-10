752ab51eda327c7f03d09054e57efc5bf9c2890c
Rich Chan <2684awer@gmail.com>
2022-06-10 11:38:22 -0500

    user

--- src/main/java/org/revature/revbook/service/UserService.java
+++ src/main/java/org/revature/revbook/service/UserService.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 @Service
 public class UserService {
 
-    @Autowired
+    final
     UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
