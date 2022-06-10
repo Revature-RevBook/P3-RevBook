e045f6770ad6c1d52f947568bf65f231a7098d0a
chandresh <chad1996air@gmail.com>
2022-06-10 13:16:55 -0500

    some changes

--- src/test/java/org/revature/revbook/RevBookApplicationTest.java
+++ src/test/java/org/revature/revbook/RevBookApplicationTest.java
@@ -7,8 +7,10 @@ import org.revature.revbook.entity.Message;
 import org.revature.revbook.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 // Use the @TestMethodOrder annotation to set the test order to be ordered numerically:
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class RevBookApplicationTest
