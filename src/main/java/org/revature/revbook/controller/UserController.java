b20336f506318fbb408f4f2717163abf1498cd6e
JohnHedler <jmhedler@gmail.com>
2022-06-12 07:10:12 -0500

    merge attempt

--- src/main/java/org/revature/revbook/controller/UserController.java
+++ src/main/java/org/revature/revbook/controller/UserController.java
@@ -3,26 +3,54 @@ package org.revature.revbook.controller;
 import org.revature.revbook.entity.User;
 import org.revature.revbook.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.validation.Valid;
+import java.util.Collections;
+import java.util.Map;
 
-// UserController Class
-// This class will handle the HTTP Requests for the API/resource paths associated with the User objects.
-@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping(path = "/users")
+@RequestMapping ("api/v1/user")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class UserController {
 
-    // PostMapping to add a User to the database:
-    @PostMapping("")
-    public Boolean addUser(@RequestBody User user) {
-        return true;
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(path = "{userId}", method = RequestMethod.GET)
+    public User getUser(@Valid @PathVariable("userId") Long userId) {
+        try {
+            return userService.getUser(userId);
+        } catch (Exception e) {
+            ResponseEntity.badRequest();
+            throw new RuntimeException(e);
+        }
     }
 
-    // GetMapping to retrieve User objects from the database:
-    @GetMapping("")
-    public String getAllUsers() {
-        return "Get All Users";
+    @GetMapping("/login")
+    public User login(@RequestParam String username, @RequestParam String password){
+        return userService.authenticate(username, password);
     }
+
+
+    @RequestMapping(path = "", method = RequestMethod.POST)
+    public User register(@Valid @RequestBody User user) {
+        return  userService.register(user);
+    }
+
+    @RequestMapping(path = "/unique", method = RequestMethod.GET)
+    public Map<String, Boolean> isUnique(@RequestParam(name = "userName") String userName,
+                                         @RequestParam(name = "userEmail") String userEmail) {
+        User user = new User(userName, userEmail);
+        Boolean isUnique = userService.isUnique(user);
+        if (isUnique) {
+            System.out.println("here");
+            return Collections.singletonMap("isUnique" , isUnique);
+        }
+        System.out.println("here");
+        return Collections.singletonMap("isUnique" , false);
+    }
+
+
 }
