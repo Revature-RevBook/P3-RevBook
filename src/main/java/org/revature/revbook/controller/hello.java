7cc743015969c208519edce23edae0c753f45042
Asad Raza Nayani <40576592+asadrazanayani@users.noreply.github.com>
2022-06-12 23:44:19 -0400

    Update hello.java

--- src/main/java/org/revature/revbook/controller/hello.java
+++ src/main/java/org/revature/revbook/controller/hello.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "")
 public class hello {
     
-    @GetMapping
+    @GetMapping(path = "")
     public String hello() {
         return "Hello";
     }
